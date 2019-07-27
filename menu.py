d90d2c0ae9412af519f5c9acfc951761cf8a5b54
oiaPY <nanfang7092180@qq.com>
2019-07-27 14:42:36 +0800

    1.增加省市县的判断 2.对应相应判断的输出

--- menu.py
+++ menu.py
@@ -19,6 +19,96 @@ dic = {"jiangsu":
        }
 
 print(dic.keys())
-# click1 = input("请输入你的选择：")
-if click1 == "jiangsu":
-     print(dic)
+while True:
+    province = input("请输入您选择的省：")
+    if province == "jiangsu":
+         print(dic["jiangsu"].keys())
+         city = input("请输入您选择的市：")
+         if city == "nanjing":
+             print(dic["jiangsu"]["nanjing"])
+             county = input("请输入您选择的区或县：")
+             if county == "gaochun":
+                print("gaochun")
+             elif county == "jiangning":
+                 print("jiangning")
+             else:
+                 print("pukou")
+         elif city == "suzhou":
+             print(dic["jiangsu"]["suzhou"])
+             county = input("请输入您选择的区或县：")
+             if county == "changshu":
+                 print("changshu")
+             elif county == "kunshan":
+                 print("kunshan")
+             else:
+                 print("wujiang")
+         else:
+             print(dic["jiangsu"]["changzhou"])
+             county = input("请输入您选择的区或县：")
+             if county == "liyang":
+                print("liyang")
+             elif county == "jintan":
+                 print("jintan")
+             else:
+                 print("xinbei")
+    elif province == "zhejiang":
+         print(dic["zhejiang"].keys())
+         city = input("请输入您选择的市：")
+         if city == "hangzhou":
+             print(dic["zhejiang"]["hangzhou"])
+             county = input("请输入您选择的区或县：")
+             if county == "yuhang":
+                print("yuhang")
+             elif county == "shangcheng":
+                 print("shangcheng")
+             else:
+                 print("xihu")
+         elif city == "jiaxing":
+             print(dic["zhejiang"]["jiaxing"])
+             county = input("请输入您选择的区或县：")
+             if county == "jiashan":
+                print("jiashan")
+             elif county == "pinghu":
+                 print("pinghu")
+             else:
+                 print("haining")
+         else:
+             print(dic["zhejiang"]["ningbo"])
+             county = input("请输入您选择的区或县：")
+             if county == "xiangshan":
+                print("xiangshan")
+             elif county == "yuyao":
+                 print("yuyao")
+             else:
+                 print("cixi")
+    else:
+         print(dic["anhui"].keys())
+         city = input("请输入您选择的市：")
+         if city == "hefei":
+             print(dic["anhui"]["hefei"])
+             county = input("请输入您选择的区或县：")
+             if county == "chaohu":
+                print("chaohu")
+             elif county == "feidong":
+                 print("feidong")
+             else:
+                 print("feixi")
+         elif city == "wuhu":
+             print(dic["anhui"]["wuhu"])
+             county = input("请输入您选择的区或县：")
+             if county == "fanchang":
+                print("fanchang")
+             elif county == "nanling":
+                 print("nanling")
+             else:
+                 print("wuwei")
+         else:
+             print(dic["anhui"]["tongling"])
+             county = input("请输入您选择的区或县：")
+             if county == "zongyang":
+                print("zongyang")
+             elif county == "tongguan":
+                 print("tongguan")
+             else:
+                 print("yi'an")
+
