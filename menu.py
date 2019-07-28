b617748561a6df4c766d3cbf90a0dcd51b4733e8
moziran <361426491@qq.com>
2019-07-28 17:48:23 +0800

    1.增加了“q”和“b”，“退出”和“返回”上一级的操作代码（但是仍有bug：退出没有问题，直接跳出循环；但是返回上一级只能打印上一级的数据，不能在上一级进行操作******） 2.增加了输入手误的情况：else情况下，打印“输入无效，请重新输入”，这个在第一级菜单可以直接用，但是往下的菜单就会返回到第一级菜单*****

--- menu.py
+++ menu.py
@@ -29,28 +29,61 @@ while True:
              county = input("请输入您选择的区或县：")
              if county == "gaochun":
                 print("gaochun")
+                break
              elif county == "jiangning":
                  print("jiangning")
-             else:
+                 break
+             elif county == "pukou":
                  print("pukou")
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["jiangsu"].keys())
+             else:
+                 print("输入无效，请重新输入！")
          elif city == "suzhou":
              print(dic["jiangsu"]["suzhou"])
              county = input("请输入您选择的区或县：")
              if county == "changshu":
                  print("changshu")
+                 break
              elif county == "kunshan":
                  print("kunshan")
-             else:
+                 break
+             elif county == "wujiang":
                  print("wujiang")
-         else:
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["jiangsu"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "changzhou":
              print(dic["jiangsu"]["changzhou"])
              county = input("请输入您选择的区或县：")
              if county == "liyang":
                 print("liyang")
+                break
              elif county == "jintan":
                  print("jintan")
-             else:
+                 break
+             elif county == "xinbei":
                  print("xinbei")
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["jiangsu"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "q":
+             break
+         elif city == "b":
+            print(dic.keys())
+         else:
+             print("输入无效，请重新输入！")
     elif province == "zhejiang":
          print(dic["zhejiang"].keys())
          city = input("请输入您选择的市：")
@@ -59,29 +92,62 @@ while True:
              county = input("请输入您选择的区或县：")
              if county == "yuhang":
                 print("yuhang")
+                break
              elif county == "shangcheng":
                  print("shangcheng")
-             else:
+                 break
+             elif county == "xihu":
                  print("xihu")
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["zhejiang"].keys())
+             else:
+                 print("输入无效，请重新输入！")
          elif city == "jiaxing":
              print(dic["zhejiang"]["jiaxing"])
              county = input("请输入您选择的区或县：")
              if county == "jiashan":
                 print("jiashan")
+                break
              elif county == "pinghu":
                  print("pinghu")
-             else:
+                 break
+             elif county == "haining":
                  print("haining")
-         else:
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["zhejiang"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "ningbo":
              print(dic["zhejiang"]["ningbo"])
              county = input("请输入您选择的区或县：")
              if county == "xiangshan":
                 print("xiangshan")
+                break
              elif county == "yuyao":
                  print("yuyao")
-             else:
+                 break
+             elif county == "cixi":
                  print("cixi")
-    else:
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["zhejiang"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "q":
+             break
+         elif city == "b":
+            print(dic.keys())
+         else:
+             print("输入无效，请重新输入！")
+    elif province == "anhui":
          print(dic["anhui"].keys())
          city = input("请输入您选择的市：")
          if city == "hefei":
@@ -89,26 +155,66 @@ while True:
              county = input("请输入您选择的区或县：")
              if county == "chaohu":
                 print("chaohu")
+                break
              elif county == "feidong":
                  print("feidong")
-             else:
+                 break
+             elif county == "feixi":
                  print("feixi")
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["anhui"].keys())
+             else:
+                 print("输入无效，请重新输入！")
          elif city == "wuhu":
              print(dic["anhui"]["wuhu"])
              county = input("请输入您选择的区或县：")
              if county == "fanchang":
                 print("fanchang")
+                break
              elif county == "nanling":
                  print("nanling")
-             else:
+                 break
+             elif county == "wuwei":
                  print("wuwei")
-         else:
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["anhui"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "tongling":
              print(dic["anhui"]["tongling"])
              county = input("请输入您选择的区或县：")
              if county == "zongyang":
-                print("zongyang")
+                 print("zongyang")
+                 break
              elif county == "tongguan":
                  print("tongguan")
-             else:
+                 break
+             elif county == "yi'an":
                  print("yi'an")
+                 break
+             elif county == "q":
+                break
+             elif county == "b":
+                 print(dic["anhui"].keys())
+             else:
+                 print("输入无效，请重新输入！")
+         elif city == "q":
+             break
+         elif city == "b":
+            print(dic.keys())
+         else:
+             print("输入无效，请重新输入！")
+    elif province == "q":
+        break
+    else:
+        print("输入无效，请重新输入！")
+
+
+
 
